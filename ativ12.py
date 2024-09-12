9ad26b5aad7f0e5f84e0c71d77524f4aaf39ce63
NaterciaMM <naterciamm2020@gmail.com>
2024-09-12 12:57:17 -0300

    first commitborameufi

---
+++ ativ12.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+
+nota = int(input("insira sua nota "))
+
+if nota >=7:
+    print("aprovado")
+elif nota <7:
+    print ("reprovado")
+
